ad830059f940750eaa7565c06dcefc5bc00466da
shinhansol <wkdqhrh010@naver.com>
2026-01-28 15:50:07 +0900

    Updates

--- _next/static/iA3AF96BrK96AVKRgqA0h/_buildManifest.js
+++ _next/static/iA3AF96BrK96AVKRgqA0h/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/daeeun-farm-privacy-policy//_next/:path+",
-        "destination": "/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
